6216156664430e939c2bda522708fb558a7fa50b
Gabriel Lessa <glessa872@gmail.com>
2024-10-16 21:34:20 -0300

    Agora as paradas tão masi avançadas

---
+++ aprendendo/variavel/atividade_const_1.js
@@ -0,0 +1,9 @@
+const cidadeNatal = "Santo Antonio de Jesus";
+
+console.log(cidadeNatal);
+
+let populacaoCidade = 10_300_000;
+
+populacaoCidade = 10_340_134;
+
+console.log(populacaoCidade);
--- aprendendo/variavel/variaveis-tipos.js
+++ aprendendo/variavel/variaveis-tipos.js
@@ -1,15 +1,18 @@
 //LEMBRAR QUE SEMPRE DEVO TRANSITAR ENTRE OS ARQUIVOS USANDO cd.. EU VOLTO PARA A PAGINA E USANDO A PAGINA 
-
+//Lembrar tambem que o codigo tem que ser escrito nessa ordem
 const primeiroNome = "Gabriel"; //Aqui a gente usa para definir variaveis constantes
 console.log(primeiroNome, "ele tenta");
 console.log(primeiroNome);
 
 //camelCase
-let idade = 19 //Aqui eu uso para poder fazer varaiveis variaveis
-console.log(idade)
-
+let idade = 19;//Aqui eu uso para poder fazer varaiveis variaveis
+//para puxar um novo valor para a variavel eu coloco o nome da variavel e dps o novo valor desejado
+idade = 20;
+console.log(idade);
+//ou seja let mutavel e const não mutavel
 let alturaEmMetros = 1.87;
-console.log(alturaEmMetros)
-// a forma de interação com o texto no java script é usandoi o ${variavel} e dentro eu coloco a variavel lembrando que tem que se escrever usando a crase
+console.log(alturaEmMetros);
 
-console.log(`${primeiroNome} e eu tenho ${alturaEmMetros} e no ano passado eu tinha ${idade}`)
+// a forma de interação com o texto no java script é usando o ${variavel} e dentro eu coloco a variavel lembrando que tem que se escrever usando a crase
+
+console.log(`${primeiroNome} e eu tenho ${alturaEmMetros} e no ano passado eu tinha ${idade}`);
