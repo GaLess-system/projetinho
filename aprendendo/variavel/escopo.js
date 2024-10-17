41659be74d030827d1d1d2edbd46df9badff1b71
Gabriel Lessa <glessa872@gmail.com>
2024-10-16 21:52:18 -0300

    Agora é hora dos loop e dos array

---
+++ aprendendo/variavel/escopo.js
@@ -0,0 +1,19 @@
+//Aqui eu não consigo puxar a variavel de dentro das chaves, mas isso só se aplica a variaveis dentro das chaves, se eu puxar o console.log de dentro com a variavel fora
+//ele ainda funciona
+{
+ const idade = 30;
+ console.log (idade);
+//Assim funciona
+}
+
+{
+    const nome = "Gabriel"
+}
+console.log(nome)
+//Assim não funiona
+
+const segundoNome = "Lessa"
+{
+console.log(segundoNome)
+//Assim funciona
+}
