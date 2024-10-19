ef8e88b8869fea5bfcb3a1e495500c116084bff7
Gabriel Lessa <glessa872@gmail.com>
2024-10-19 17:40:17 -0300

    Aqui já tem mais coisas aprendidas

---
+++ aprendendo/javascript/contas.js
@@ -0,0 +1,18 @@
+let x = 10, y = 2;
+
+let resultado = y + x;
+console.log(`A soma de ${x} + ${y} é igual á ${resultado}`);
+
+resultado = y * x;
+console.log(`A multiplicação de ${x} * ${y} é ${resultado}`);
+
+resultado = x / y;
+console.log(`A divisão de ${x} / ${y} é ${resultado}`)
+
+//Para decrementar e incrementar em um o valor eu pego, como exemplo X e uso os operandores --x ou ++x aqui incrementa
+x--;// aqui decrementa por exemplo
+console.log(x); 
+
+//raiz quadrada a gente usa
+let raizX = Math.sqrt(x);
+console.log(raizX);
